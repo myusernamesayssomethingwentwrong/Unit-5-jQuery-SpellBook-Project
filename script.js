f5d84ef2dfb69063616a148ae2f944ecfc940690
myusernamesayssomethingwentwrong <43214459+myusernamesayssomethingwentwrong@users.noreply.github.com>
2019-03-06 08:58:53 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+//Spell #1
+
+$(".Cook").click(function(){
+	$(".Chicken").slideUp();
+});
+
+//Spell #2
+
+$(".Done").click(function(){
+    $(".McChicken").slideDown();
+});
+
+//Spell #3
+
+$(".Reset").click(function(){
+    $(".McChicken").slideUp();
+});
+
+
